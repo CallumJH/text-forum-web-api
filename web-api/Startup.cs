844a68bd2b0fe58527e5ed25c7d54e2ab35b3eb7
Callum John Harris <ch1397@gmail.com>
2024-01-20 16:31:35 +0200

    Added some notes
    + Further extra features
    - removed some boilerplate comments

--- web-api/Startup.cs
+++ web-api/Startup.cs
@@ -14,12 +14,8 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        //using LinqToDB.AspNet
         services.AddLinqToDBContext<DataBaseConnection>((provider, options)
             => options
-                //will configure the AppDataConnection to use
-                //sqite with the provided connection string
-                //there are methods for each supported database
                 .UseSQLite(Configuration.GetConnectionString("Default"))
                 //default logging will log everything using the ILoggerFactory configured in the provider
                 .UseDefaultLogging(provider));
