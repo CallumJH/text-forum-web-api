b5966ec6dc15026c4ead87c0786bf604cf6f27b4
Callum John Harris <ch1397@gmail.com>
2024-01-20 15:10:43 +0200

    Created Posts model and controller
    + Posts controller for post related actions
    + post model for post information when created

--- web-api/Models/Post.cs
+++ web-api/Models/Post.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 
 /// <summary>
-/// Post model for creating posts this is not to be confused with the POCO model.
+/// Post model for creating posts this is not to be confused with the Post DataModel.
 /// </summary>
 public class Post
 {   
@@ -17,7 +17,7 @@ public class Post
     [Required]
     [RegularExpression(@"^[a-zA-Z0-9_-]{3,3000}$")]
     public string Content { get; set; }
-    
+
     /// <summary>
     /// DateCreated is set to the current date and time when the post is created keeping all values to UTC.
     /// </summary>
